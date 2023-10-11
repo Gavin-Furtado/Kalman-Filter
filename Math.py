964545c53e786120eff8aabdc5c5eceba0386739
Gavin Furtado <gavin98007@gmail.com>
2023-10-11 15:32:03 +0100

    Added new functions

--- Math.py
+++ Math.py
@@ -1,9 +1,40 @@
 ## This script consists of all the mathematical formulae 
 ## required for Kalman filter calculations
 
-
-def kalman_gain(E_est,E_mea):
+## Math equation ##
+## KG = Eest / (Eest + Emea) ##
+def kalman_gain(E_est:float,E_mea:float)->float:
+    '''This function calculates the Kalman gain
+    input: Error in estimate, Error in measurement
+    output: Kalman gain
+    '''
     gain = E_est/(E_est + E_mea)
     return  gain
 
+## Math equation ##
+## Current Estimate = Previous extimate + KG[measurement - previous estimate] ##
+def current_estimate(previous_est:float,
+                      measurement:float,
+                      kalmangain:float)->float:
+   '''
+   This function calcuates the current esitmate for the kalman filter
+   input: previous estimate, currrent measurement, kalman gain
+   output: current estimate
+   '''
+   residual = measurement - previous_est
+   weighted_residue = kalmangain * residual 
+   return (previous_est + weighted_residue)
 
+## Math equation ##
+## Error in current estimate = [1 - KG] x error in previous estimate ##
+def error_in_estimate(kalmangain:float,error_previous_est:float)->float:
+    '''
+    Calulates the error in current estimate
+    input: Kalman Gain, Error in previous estimate
+    output: Error in current estimate
+    '''
+    return (1-kalmangain)*error_previous_est
+
+
+gain = kalman_gain(4,6)
+print("Kalman gain=" +str(gain))
