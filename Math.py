9c95025739c63bd22ad733c1e17dbd8a603f3d16
Gavin Furtado <gavin98007@gmail.com>
2023-10-11 14:45:56 +0100

    Title comments

--- Math.py
+++ Math.py
@@ -1,6 +1,9 @@
+## This script consists of all the mathematical formulae 
+## required for Kalman filter calculations
+
 
 def kalman_gain(E_est,E_mea):
     gain = E_est/(E_est + E_mea)
     return  gain
 
-# new branch testing
+
