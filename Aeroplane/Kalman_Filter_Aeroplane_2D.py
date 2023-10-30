33aca3fa82d1859b058c20c5ec03de339cb6756b
Gavin Furtado <gavin98007@gmail.com>
2023-10-30 14:53:46 +0000

    updated the graph

--- Aeroplane/Kalman_Filter_Aeroplane_2D.py
+++ Aeroplane/Kalman_Filter_Aeroplane_2D.py
@@ -6,8 +6,8 @@ measurement_position = [4000,4260,4550,4860,5110]
 measurement_velocity = [280,282,285,286,290]
 
 ## Estimated values from Kalman filter ##
-estimates = {'position':[], 'velocity':[]}
-print(estimates)
+prediction = {'position':[], 'velocity':[]}
+kalman_filter = {'position':[], 'velocity':[]}
 
 ## Step - Initialization ##
 initial_state_matrix = X_initial(4000,280)
@@ -87,6 +87,10 @@ for i in range(5):
     else:
         predicted_process_covariance_matrix = P_predicted(1,previous_process_covariance_matrix)
 
+    ## storing prediction value in dictionary ##
+    prediction['position'].append(predicted_state_matrix[0][0])
+    prediction['velocity'].append(predicted_state_matrix[1][0])
+
     ## Step 2 - Measurement/Data/Observation from sensors ##
     measured_values = measurements(measurement_position[i],measurement_velocity[i])
 
@@ -101,9 +105,9 @@ for i in range(5):
     updated_process_covariance_matrix = calculate_updated_process_covariance(kalman_gain_matrix,matrix_H(),
                                                                             predicted_process_covariance_matrix)
     
-    ## Updating estimates dictionary
-    estimates['position'].append(updated_state_matrix[0][0])
-    estimates['velocity'].append(updated_state_matrix[1][0])
+    ## storing kalman filter values in dictionary ##
+    kalman_filter['position'].append(updated_state_matrix[0][0])
+    kalman_filter['velocity'].append(updated_state_matrix[1][0])
 
     ## Updated become previous ##
     previous_state_matrix = updated_state_matrix
@@ -134,9 +138,11 @@ for i in range(5):
 
 
 ## Plotting graphs ##
-plt.plot(measurement_position,measurement_velocity,'g^')
-plt.plot(estimates['position'],estimates['velocity'],'ro')
+plt.plot(measurement_position,measurement_velocity)
+plt.plot(prediction['position'],prediction['velocity'])
+plt.plot(kalman_filter['position'],kalman_filter['velocity'])
+plt.grid(which='both')
 plt.xlabel('Position (m)')
 plt.ylabel('Velocity (m/s^2)')
-plt.legend('measured values')
+plt.legend(['measurments','predictions','kalman filter'])
 plt.show()
