f44d44e1c2eb8a189ff8467ac9363d872f9c704e
Gavin Furtado <gavin98007@gmail.com>
2023-10-30 15:35:53 +0000

    new graphs

--- Aeroplane/Kalman_Filter_Aeroplane_2D.py
+++ Aeroplane/Kalman_Filter_Aeroplane_2D.py
@@ -10,32 +10,32 @@ prediction = {'position':[], 'velocity':[]}
 kalman_filter = {'position':[], 'velocity':[]}
 
 ## Step - Initialization ##
-initial_state_matrix = X_initial(4000,280)
+initial_state_matrix = X_initial(measurement_position[0],measurement_velocity[0])
 initial_control_matrix = U_initial(2)
 initial_noise_matrix = W_initial(0)
 initial_process_covariance_matrix = P_initial(20,5)
 
-## Step 1 - Prediction ##
-# previous_state_matrix = updated_current_state_matrix
-predicted_state_matrix = X_predicted(1,initial_state_matrix,
-                                     initial_control_matrix, initial_noise_matrix)
+# ## Step 1 - Prediction ##
+# # previous_state_matrix = updated_current_state_matrix
+# predicted_state_matrix = X_predicted(1,initial_state_matrix,
+#                                      initial_control_matrix, initial_noise_matrix)
 
-# previous_process_covariance_matrix = updated_process_covariance_state_matrix
-predicted_process_covariance_matrix = P_predicted(1,initial_process_covariance_matrix)
+# # previous_process_covariance_matrix = updated_process_covariance_state_matrix
+# predicted_process_covariance_matrix = P_predicted(1,initial_process_covariance_matrix)
 
-## Step 2 - Measurement/Data/Observation from sensors ##
-measured_values = measurements(4260,282)
+# ## Step 2 - Measurement/Data/Observation from sensors ##
+# measured_values = measurements(4260,282)
 
-## Step 3 - Kalman Gain ##
-kalman_gain_matrix = Kalman_Matrix(predicted_process_covariance_matrix,25,6)
+# ## Step 3 - Kalman Gain ##
+# kalman_gain_matrix = Kalman_Matrix(predicted_process_covariance_matrix,25,6)
 
-## Step 4 - Updation ##
-# Updated Current State Matrix
-updated_state_matrix =calculate_updated_state_matrix(matrix_H(),measured_values,kalman_gain_matrix,predicted_state_matrix)
+# ## Step 4 - Updation ##
+# # Updated Current State Matrix
+# updated_state_matrix =calculate_updated_state_matrix(matrix_H(),measured_values,kalman_gain_matrix,predicted_state_matrix)
 
-# Updated Process Covariance Matrix
-updated_process_covariance_matrix = calculate_updated_process_covariance(kalman_gain_matrix,matrix_H(),
-                                                                         predicted_process_covariance_matrix)
+# # Updated Process Covariance Matrix
+# updated_process_covariance_matrix = calculate_updated_process_covariance(kalman_gain_matrix,matrix_H(),
+#                                                                          predicted_process_covariance_matrix)
 
 ### Printing 1st loop
 # print(f'=========Initialization==================')
@@ -138,11 +138,24 @@ for i in range(5):
 
 
 ## Plotting graphs ##
-plt.plot(measurement_position,measurement_velocity)
-plt.plot(prediction['position'],prediction['velocity'])
-plt.plot(kalman_filter['position'],kalman_filter['velocity'])
+plt.plot([0,1,2,3,4],measurement_position,'ro')
+plt.plot([0,1,2,3,4],prediction['position'],'g^')
+plt.plot([0,1,2,3,4],kalman_filter['position'],'r-')
 plt.grid(which='both')
-plt.xlabel('Position (m)')
+plt.xlabel('Time period')
+plt.ylabel('Position (m)')
+plt.legend(['measurments','predictions','kalman filter'])
+plt.show()
+
+# velocity graph
+plt.plot([0,1,2,3,4],measurement_velocity,'ro')
+plt.plot([0,1,2,3,4],prediction['velocity'],'g^')
+plt.plot([0,1,2,3,4],kalman_filter['velocity'],'r-')
+plt.grid(which='both')
+plt.xlabel('Time period')
 plt.ylabel('Velocity (m/s^2)')
 plt.legend(['measurments','predictions','kalman filter'])
 plt.show()
+
+print(prediction)
+print(kalman_filter)
