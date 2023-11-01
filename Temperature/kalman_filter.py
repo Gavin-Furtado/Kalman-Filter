962d395c6dd8871f43d20dbd3ed2cdc8b7908d82
Gavin Furtado <gavin98007@gmail.com>
2023-11-01 12:53:17 +0000

    fixing trailing white space

--- Temperature/kalman_filter.py
+++ Temperature/kalman_filter.py
@@ -11,20 +11,21 @@ Reference: Michel Van Biezen lectures
 from math_functions import kalman_gain, current_estimate, error_in_estimate
 
 #########################################################
-### Solving a one dimensional temperature example
-### True temperature = 72
-### Initial estimate = 68
-### Initial error in estimate = 2
-### Inital measurement = 75
-### Error in measurement = 4  
+# Solving a one dimensional temperature example
+# True temperature = 72
+# Initial estimate = 68
+# Initial error in estimate = 2
+# Inital measurement = 75
+# Error in measurement = 4
 #########################################################
 
 #####################################################################################
 ### Inital variables ###
-ERROR_IN_EST = 2 #The inital error in estimate
-ERROR_IN_MEA = 4 #The error in measurement
-PREVIOUS_EST = 68 # intial previous estimate
-temperature_measurement = [75,71,70,74,70,75,71,76,74,74] # list of measurements
+ERROR_IN_EST = 2  # The inital error in estimate
+ERROR_IN_MEA = 4  # The error in measurement
+PREVIOUS_EST = 68  # intial previous estimate
+temperature_measurement = [75, 71, 70, 74, 70,
+                           75, 71, 76, 74, 74]  # list of measurements
 #####################################################################################
 
 
@@ -32,18 +33,18 @@ temperature_measurement = [75,71,70,74,70,75,71,76,74,74] # list of measurements
 #####################################################################################
 for m in temperature_measurement:
     ### Calucaltion loop ###
-    gain = kalman_gain(ERROR_IN_EST,ERROR_IN_MEA) #Kalman Gain
-    crr_est = current_estimate(PREVIOUS_EST,m,gain) # Current estimate
-    err_est = error_in_estimate(gain,ERROR_IN_EST) #Error in current estimate
-    
+    gain = kalman_gain(ERROR_IN_EST, ERROR_IN_MEA)  # Kalman Gain
+    crr_est = current_estimate(PREVIOUS_EST, m, gain)  # Current estimate
+    # Error in current estimate
+    err_est = error_in_estimate(gain, ERROR_IN_EST)
+
     ### Updation of variable ###
     ERROR_IN_EST = err_est
     PREVIOUS_EST = crr_est
 
     ### Display loop ###
-    print("   " + "Kalman Gain = " + str(gain) + 
-          "||" + "Current Estimate = " + str(crr_est)+ 
+    print("   " + "Kalman Gain = " + str(gain) +
+          "||" + "Current Estimate = " + str(crr_est) +
           "||" + "Error in Estimate = " + str(err_est))
     print("=========================================================================")
 ######################################################################################
-
