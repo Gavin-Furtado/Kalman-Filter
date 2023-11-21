841a73b830bbd4a7a4cdfd1e0025931b28ed6142
Gavin Furtado <gavin98007@gmail.com>
2023-11-21 14:49:39 +0000

    Plotted the sensor graph

--- Sensor data/kf_test.py
+++ Sensor data/kf_test.py
@@ -43,6 +43,8 @@ Author: Gavin Furtado
 
 
 from numpy.random import randn
+import matplotlib.pyplot as plt
+import numpy as np
 
 class PosSensor(object):
     def __init__(self, pos=(0,0), vel=(0,0), noise_std=1.):
@@ -56,3 +58,9 @@ class PosSensor(object):
 
         return [self.pos[0] + randn() * self.noise_std,
                 self.pos[1] + randn() * self.noise_std]
+    
+pos,vel =(4,3), (2,1)
+sensor = PosSensor(pos, vel, noise_std=1)
+ps = np.array([sensor.read() for _ in range(50)])
+plt.scatter(ps[:,0], ps[:,1])
+plt.show()
