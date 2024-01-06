85c8dc65d0423303ccc3f0b2875b23da856d3291
Gavin Furtado <gavin98007@gmail.com>
2024-01-06 14:19:35 +0000

    Dummy data class

--- Phase 2/filter_py_lib_test/kf_test.py
+++ Phase 2/filter_py_lib_test/kf_test.py
@@ -17,7 +17,9 @@ Author: Gavin Furtado
 import math
 import numpy as np
 from numpy.random import randn
+import matplotlib.pyplot as plt
 
+## dummy data - tracking a dog
 def compute_dog_data(measurement_var, process_var, count=1, dt=1):
     # initial value for position and velocity
     position, velocity = 0.,1.
@@ -38,4 +40,48 @@ def compute_dog_data(measurement_var, process_var, count=1, dt=1):
 
 print(compute_dog_data(0.5,0.2))
 
+## dummy data class - tracking a robot
+## A model of real world object: electronic sensor
+class PositionSensor(object):
+    '''
+    Attributes: self, position, velocity, noise standard deviation
+    
+    Methods: read
+    '''
+    def __init__(self, initial_position=(0.,0.), initial_velocity=(0.,0.), acceleration=(0.1,0.4), noise_std=1.) -> None:
+        self.position = np.array(initial_position)
+        self.velocity = np.array(initial_velocity)
+        self.acceleration = np.array(acceleration)
+        self.noise_std = noise_std
 
+    def read(self):
+        self.velocity += self.acceleration
+        self.position += self.velocity
+
+        return self.position + np.random.randn() * self.noise_std
+
+## Initialise the Sensor
+sensor = PositionSensor()
+
+# Genertion of dummy data
+sample_size = 10
+position_data = np.zeros((sample_size,2))
+velocity_data = np.zeros((sample_size,2))
+
+for i in range(sample_size):
+    sensor.read()
+    position_data[i] = sensor.position
+    velocity_data[i] = sensor.velocity
+
+time_interval = np.arange(sample_size) # Create an array for x-axis, use either range() or np.arange()
+print(position_data)
+plt.scatter(time_interval,position_data[:,0],label = 'X-Position')
+plt.scatter(time_interval,position_data[:,1],label = 'Y-Position')
+#plt.plot(time_interval,velocity_data[:,0],label = 'X-Velocity')
+#plt.plot(time_interval,velocity_data[:,1],label = 'Y-Velocity')
+plt.legend()
+plt.show()
+
+## Using FilterPy library
+
+## Without FilterPy library
