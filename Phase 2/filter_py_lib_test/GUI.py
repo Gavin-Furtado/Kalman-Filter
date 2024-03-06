64d8d073403b5e91ba6a259ec065c29041a85ec6
Gavin Furtado <gavin98007@gmail.com>
2024-03-06 16:51:01 +0000

    Links

--- Phase 2/filter_py_lib_test/GUI.py
+++ Phase 2/filter_py_lib_test/GUI.py
@@ -1,3 +1,12 @@
+'''
+https://www.youtube.com/watch?v=Miydkti_QVE 
+
+https://www.youtube.com/playlist?list=PLfZw_tZWahjxJl81b1S-vYQwHs_9ZT77f 
+
+https://www.youtube.com/watch?v=0H19QzbBqC4 
+
+'''
+
 import tkinter
 import customtkinter as ctk
 import kf_algorithm
@@ -7,7 +16,7 @@ def RunProgram():
         val_1 = int(sample_size.get())
         kf_algorithm.main(val_1)
     except ValueError:
-        ctk.messagebox.showerror('Error','Please Enter a valid number')
+        tkinter.messagebox.showerror('Error','Please Enter a valid number')
 
 
 
@@ -20,16 +29,32 @@ app = ctk.CTk()
 app.geometry('720x480')
 app.title('Kalman Filter Algorithm')
 
+font_1 = ctk.CTkFont(family='Calibri',size=14, weight='bold')
+
 #Adding UI elements
-label_1=ctk.CTkLabel(app, text="Sample Size")
+# Sample Size
+label_1=ctk.CTkLabel(app, text="Sample Size",font=font_1)
 label_1.pack(padx=10,pady=10)
+label_1.place(relx=0.1,rely=0.1,anchor=tkinter.CENTER)
 
 sample_size = ctk.CTkEntry(app,width=200, height=30)
 sample_size.pack(padx=10,pady=10)
+sample_size.place(relx=0.3,rely=0.1,anchor=tkinter.CENTER)
+
+# Noise Mean
+# label_2 = ctk.CTkLabel(app,text='Mean value of Noise')
+
+
+# Noise Standard Deviation
+
+
 
 # Plot Button
-plot = ctk.CTkButton(app, text="Plot Graph",command=RunProgram)
+plot = ctk.CTkButton(app, text="Plot Graph", corner_radius=32, command=RunProgram,
+                     fg_color='#0096C7',hover_color='#00B4D8',border_color='#023E8A',
+                     border_width=1.5, font=font_1)
 plot.pack(padx=10,pady=10)
+plot.place(relx=0.5,rely=0.8,anchor=tkinter.CENTER)
 
 # Run app
 app.mainloop()
